def45f5135e8becbb7119939b0319e4d51489732
L D <delayel06@gmail.com>
2024-11-18 10:24:56 +0100

    b

--- qiskitML.py
+++ qiskitML.py
@@ -1,89 +1,65 @@
-from qiskit_algorithms.utils import algorithm_globals
-
-algorithm_globals.random_seed = 12345
-
-from qiskit_machine_learning.datasets import ad_hoc_data
-
-adhoc_dimension = 2
-train_features, train_labels, test_features, test_labels, adhoc_total = ad_hoc_data(
-    training_size=20,
-    test_size=5,
-    n=adhoc_dimension,
-    gap=0.3,
-    plot_data=False,
-    one_hot=False,
-    include_sample_total=True,
-)
-
-import matplotlib.pyplot as plt
 import numpy as np
+from qiskit import QuantumCircuit
+
+#fonction qui fait la matrice U du kernel avec des portes H, Rz(features), Ry
+#Rz tourne de la valeur de la feature x1, puis x2. 2 qubit par point car chaque point a 2 feature
+def U(point, thetas, qc):
+    pattern = [0,1,0,1,0]
+    num_qubits = 5
+
+    x1, x2, label = point
+
+    for j in range(5):
+        
+        qc.h(j)
+        print ("added gate H on qubit", j)
+
+        if pattern[j] == 0:
+            qc.rz(x1,j)
+            print("added gate Rz on qubit", j, "with angle", x1)
+        else:
+            qc.rz(x2,j)
+            print("added gate Rz on qubit", j, "with angle", x2)
+
+        qc.ry(thetas[j],j)
+        print("added gate Ry on qubit", j, "with angle", thetas[j % 5])
+
+        #add crz with angles theta 6-10 on each qubit, controlled by the previous. first qubit is controlled by last
+    
+        qc.crz(thetas[j + 5], j-1, j)
+        print("added gate crz on qubit", j, "controlled by qubit", j-1, "with angle", thetas[j + 5])
+
+    return qc
+
+def U_(point, thetas, qc):
+
+    pattern = [0, 1, 0, 1, 0]
+    num_qubits = 5
+    
+    
+    x1, x2, label = point
+    
+    for j in range(5):  
+        
+        
+        
+        qc.crz(-thetas[j + 5], j-1, j)
+            
+        
+        qc.ry(-thetas[j % 5], j)
+        
+        if pattern[j] == 0:
+            qc.rz(-x1, j)
+        else:
+            qc.rz(-x2, j)
+            
+        
+        qc.h(j)
+    
+    return qc
 
 
-def plot_features(ax, features, labels, class_label, marker, face, edge, label):
-    # A train plot
-    ax.scatter(
-        # x coordinate of labels where class is class_label
-        features[np.where(labels[:] == class_label), 0],
-        # y coordinate of labels where class is class_label
-        features[np.where(labels[:] == class_label), 1],
-        marker=marker,
-        facecolors=face,
-        edgecolors=edge,
-        label=label,
-    )
-
-
-def plot_dataset(train_features, train_labels, test_features, test_labels, adhoc_total):
-
-    plt.figure(figsize=(5, 5))
-    plt.ylim(0, 2 * np.pi)
-    plt.xlim(0, 2 * np.pi)
-    plt.imshow(
-        np.asmatrix(adhoc_total).T,
-        interpolation="nearest",
-        origin="lower",
-        cmap="RdBu",
-        extent=[0, 2 * np.pi, 0, 2 * np.pi],
-    )
-
-    # A train plot
-    plot_features(plt, train_features, train_labels, 0, "s", "w", "b", "A train")
-
-    # B train plot
-    plot_features(plt, train_features, train_labels, 1, "o", "w", "r", "B train")
-
-    # A test plot
-    plot_features(plt, test_features, test_labels, 0, "s", "b", "w", "A test")
-
-    # B test plot
-    plot_features(plt, test_features, test_labels, 1, "o", "r", "w", "B test")
-
-    plt.legend(bbox_to_anchor=(1.05, 1), loc="upper left", borderaxespad=0.0)
-    plt.title("Ad hoc dataset")
-
-    plt.show()
-
-plot_dataset(train_features, train_labels, test_features, test_labels, adhoc_total)
-
-from qiskit.circuit.library import ZZFeatureMap
-from qiskit.primitives import Sampler
-from qiskit_algorithms.state_fidelities import ComputeUncompute
-from qiskit_machine_learning.kernels import FidelityQuantumKernel
-
-adhoc_feature_map = ZZFeatureMap(feature_dimension=adhoc_dimension, reps=2, entanglement="linear")
-
-sampler = Sampler()
-
-fidelity = ComputeUncompute(sampler=sampler)
-
-adhoc_kernel = FidelityQuantumKernel(fidelity=fidelity, feature_map=adhoc_feature_map)
-
-from sklearn.svm import SVC
-
-adhoc_svc = SVC(kernel=adhoc_kernel.evaluate)
-
-adhoc_svc.fit(train_features, train_labels)
-
-adhoc_score_callable_function = adhoc_svc.score(test_features, test_labels)
-
-print(f"Callable kernel classification test score: {adhoc_score_callable_function}")
+    
+    
+    
+ 
